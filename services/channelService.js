af63ca4e4758f0f7af5d9e0dea898ced05b35301
giho <giho7427@daum.net>
2024-06-25 14:48:28 +0900

    feat: channel에 owner 추가

--- services/channelService.js
+++ services/channelService.js
@@ -29,7 +29,8 @@ const getChannelById = async (req, res) => {
 
 // 채널 생성하기
 const createChannel = async (req, res) => {
-  const { name, description, image, tags } = req.body.channel;
+  const { name, description, image, tags, ownerId } = req.body.channel;
+  console.log(req.body.channel);
   try {
     const newChannel = await prisma.channel.create({
       data: {
@@ -37,6 +38,7 @@ const createChannel = async (req, res) => {
         description,
         image,
         tags,
+        ownerId,
       },
     });
     return res.json(newChannel);
