192884cf157a1033c3f521ce85dd92bfeb61da4a
giho <giho7427@daum.net>
2024-07-17 02:45:13 +0900

    chore: 배포 테스트

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import channelRoutes from '../routes/channelRoutes.js';
 import musicRoutes from '../routes/musicRoutes.js';
 import authRoutes from '../routes/authRoutes.js';
 
-// 배포 테스트
+// 배포 테스트 제발 되라!
 const app = express();
 const PORT = process.env.PORT || 8080;
 
